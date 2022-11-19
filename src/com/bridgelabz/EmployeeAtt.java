304861849be273a6cc391ea60ff70d5488fcd872
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-11-19 20:00:11 +0530

    Check Employee are present or Absent

---
+++ src/com/bridgelabz/EmployeeAtt.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class EmployeeAtt {
+    public static void main(String[] args) {
+            int IS_FULL_TIME = 1;
+            double empCheck = Math.floor(Math.random() * 10.0) % 2.0;
+            if (empCheck == (double)IS_FULL_TIME) {
+                System.out.println("Employee is present");
+            } else {
+                System.out.println("Employee is absent");
+            }
+
+        }
+    }
+
